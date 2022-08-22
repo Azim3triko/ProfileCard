b97044a4161cb605c3ebad7f7fd0b11f31fc42a1
Azim3triko <azim3triko@protonmail.com>
2022-08-22 20:14:49 +0000

    se arregla proyecto para aprobar

--- src/js/app.js
+++ src/js/app.js
@@ -36,22 +36,22 @@ function render(variables = {}) {
           <h1>${variables.name === null ? "" : variables.name} ${
     variables.lastname === null ? "" : variables.lastname
   }</h1>
-          <h2>${variables.role === null ? "" : variables.role}</h2>
-          <h3>${variables.city === null ? "" : variables.city} ${
-    variables.country === null ? "" : variables.country
+          <h2>${variables.role === null ? "role" : variables.role}</h2>
+          <h3>${variables.city === null ? "ciudad" : variables.city} ${
+    variables.country === null ? "pais" : variables.country
   }</h3>
-          <ul class="position-right">
-            <li><a href="${
-              variables.twitter === null ? "" : variables.twitter
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : ""
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${
-              variables.github === null ? "" : variables.github
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="${
-              variables.linkedin === null ? "" : variables.linkedin
+            <li><a href="https://linkedin.com/${
+              variables.linkedin ? variables.linkedin : ""
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${
-              variables.instagram === null ? "" : variables.instagram
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : ""
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
